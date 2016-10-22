f91e730a2884fe93c2ec9327ca8e7c195c9af95c
cvilson <cvilson@hotmail.com>
2016-10-22 12:17:49 -0200

    Update Picture_model.php

--- application/models/Picture_model.php
+++ application/models/Picture_model.php
@@ -1,14 +1,14 @@
-<?php
-Class Picture_model extends CI_Model{
-	public function getPostPictures($postId, $offset = 0, $limit = 50){
-		$query = $this->db
-			->select('*')
-			->from("pictures")
-			->where('id_post', $postId)
-			->order_by('create_date', 'DESC')
-			->limit($limit, $offset)->get();
-
-		return $query->result_array();
-	}
-}
-?>
+<?php 
+Class Pictures_model extends CI_Model{ 
+        public function getAllPictures($offset = 0, $limit = 50){ 
+                $query = $this->db->select('*')->from("pictures")->order_by('create_date', 'DESC')->limit($limit, $offset)->get(); // seleciona todas as figuras por ordem de data de criacao
+ 
+                return $query->result_array(); 
+        } 
+ 
+        public function getCountAllPictures(){ 
+                 return $this->db->count_all("pictures"); 
+         }        
+   
+ 
+ ?>
