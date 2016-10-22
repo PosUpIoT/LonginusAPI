1e7f5ffd27f40258bc918a765dc3cfd2427be641
Antonio Guilherme Luz <aga2017@hotmail.com>
2016-10-22 12:26:37 -0200

    Update Picture_model.php

--- application/models/Picture_model.php
+++ application/models/Picture_model.php
@@ -8,7 +8,25 @@ Class Pictures_model extends CI_Model{
  
         public function getCountAllPictures(){ 
                  return $this->db->count_all("pictures"); 
-         }        
-   
- 
+         }  
+        
+         	public function getPicturesSearch($idPicture, $offset = 0, $limit= 50){ 
+  
+ 		$query = $this->db->distinct()->select('*', FALSE)->from('pictures')->where('id_post = '.$idPicture)->limit($limit, $offset)->get(); 
+  
+  
+ 		return $query->result_array(); 
+  
+ 	}	 
+  
+ 	public function getPictures($id){ 
+ 		$data = array('id' => $id); 
+ 		$query = $this->db->get_where('pictures',$data); 
+ 		return $query->row(); 
+ 	}	 
+  
+ 	public function newPictures($data){  						// adicionar nova figura 
+ 		return $this->db->insert('pictures', $data); 
+ 	} 
+ } 
  ?>
