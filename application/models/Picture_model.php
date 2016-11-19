f2e0f13ee17e450d9448efc782cbc57bbd4b7b6a
Giovane Ferreira <giovaneferreira93@gmail.com>
2016-11-19 10:11:50 -0200

    Ajuste no nome da classe

--- application/models/Picture_model.php
+++ application/models/Picture_model.php
@@ -1,5 +1,5 @@
 <?php 
-Class Pictures_model extends CI_Model{ 
+Class Picture_model extends CI_Model{ 
         public function getAllPictures($offset = 0, $limit = 50){ 
                 $query = $this->db->select('*')->from("pictures")->order_by('create_date', 'DESC')->limit($limit, $offset)->get(); // seleciona todas as figuras por ordem de data de criacao
  
